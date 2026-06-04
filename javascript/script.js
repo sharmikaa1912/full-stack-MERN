5cc248eb07a3db636985858890767aaa48634da1
sharmika <sharmika543@gmail.com>
2026-06-04 15:40:01 +0530

    js variablrs , data types, operators done

---
+++ javascript/script.js
@@ -0,0 +1,105 @@
+// var a = 10; //Reinitialization & Redeclare
+// let b = 20; //Reinitialize
+// const c = 30; 
+
+// const { cacheSignal } = require("react");
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// var num = 10;
+// var str = "sharmika";
+// var undefined;
+// var Null = null;
+// var boolean = true;
+
+// console.log(typeof num);
+// console.log(typeof str);
+// console.log(typeof undefine);
+// console.log(typeof Null);
+// console.log(typeof boolean);
+
+
+// var bigInt = 123n;
+// var symbol = Symbol(' li ');
+// console.log(typeof bigInt)
+// console.log(typeof symbol)
+
+// var arr = [10,20,30,40]
+// console.log( typeof arr , arr);
+// var obj = {
+// //     name:"sharmika",
+// //     dept:[`AI&DS`,"ct"]
+// // }
+// // console.log(typeof obj,obj)
+
+// //Arithmetic operator (+,-,*,%,/)
+// var a = 1
+// var b = 2;
+
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a % b);
+// console.log(a / b);
+
+// //logical operator (&,||,!)
+//                 &&       ||
+// // true true    true    true
+// // true false   false   true
+// // false true   false   true
+// // false false  false   false
+
+// var a = true;
+// var b = false;
+
+// console.log(a&&b);
+// console.log(a||b);
+// console.log(!a);
+
+//Relational operator(>,>=,<,<=,==,!=)
+
+// a = 10; 
+// b = "10";
+
+// console.log(a>b);
+// console.log(a<b);
+// console.log(a>=b);
+// console.log(a<=b);
+// console.log(a==b);
+// console.log(a!=b);
+// console.log(a===b);
+// console.log(a!==b);
+
+//Assingment operator (=,+=,-=,%=,*=./=,**=)
+// a = 10;
+// b = 20;
+
+// a += b;
+// console.log(a)
+// a -= b;
+// console.log(a)
+
+
+//Unary operator(--,++)
+
+// var a = 10;
+// console.log(a++);
+// console.log(++a);
+
+// var a = 10;
+// console.log(a--);
+// console.log(--a);
+
+a = 1;
+b = 1;
+c = 0;
+
+var result = a++ + --b + ++c - ++a + ++b + c++ 
++ b++ + a;
+
+console.log(result)
+
+
+
